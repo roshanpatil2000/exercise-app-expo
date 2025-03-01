ac7bb12486d8799f5259d798cb99b4d2246bb5a4
roshanpatil2000 <roshanpatil2912.rp@gmail.com>
2025-03-01 22:10:48 +0530

    project setup

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View>
+      <Text>Our First screen</Text>
+    </View>
+  )
+}
+
+export default index
+
